d213ce10dea18c39b45f1d62d66a6ea947340343
Martin Mikael Yrjola <yrjola@kth.se>
2017-05-04 15:49:01 +0200

    More extensive setup for the working_set_size.sh script

--- working_set_size.sh
+++ working_set_size.sh
@@ -4,9 +4,9 @@ set -e
 
 # Exercise 3.3 Find out working set size for the benchmark programs.
 
-for benchmark in parser
+for benchmark in equake vortex parser
 do
-    for cache_lines in 1048576
+    for cache_lines in 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576
     do
         ASSOC=1 BENCHMARK=$benchmark CACHE_LINES=$cache_lines ./simics -stall -no-stc -c ${benchmark}.conf -no-win -q -p collect_cache_statistics.py
         ASSOC=4 BENCHMARK=$benchmark CACHE_LINES=$cache_lines ./simics -stall -no-stc -c ${benchmark}.conf -no-win -q -p collect_cache_statistics.py
