6dea2788713f753bbd698aeeda58d64ba461ba4a
Martin Mikael Yrjola <yrjola@kth.se>
2017-05-10 10:39:27 +0200

    Add results and scripts for 3.4 and 3.5

---
+++ Lab1_3-5/cache_hierarchy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# Exercise 3.3 Find out working set size for the benchmark programs.
+
+for benchmark in equake vortex parser
+do
+    for cache_lines in 32 4096 8192
+    do
+        BENCHMARK=$benchmark CACHE_LINES=$cache_lines ./simics -stall -no-stc -c ${benchmark}.conf -no-win -q -p Lab1_3-5/cache_hierarchy.py
+    done
+done
