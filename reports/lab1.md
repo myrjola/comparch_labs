f514ae6b81acbeee302e18604e44c20aa742cfa5
Martin Yrjölä <martin.yrjola@gmail.com>
2017-05-11 10:25:28 +0200

    Add report

---
+++ reports/lab1.md
@@ -0,0 +1,110 @@
+---
+title: Laboratory 1
+author:
+- Yrkkö Äkkijyrkkä <yrkko@kth.se>
+- Martin Yrjölä <yrjola@kth.se>
+bibliography: references.bib
+---
+
+3.2 Collecting statistics from simple cache
+===========================================
+
+The performance results are given in @tbl:cache_statistics_results. It is clear
+that the parser benchmark has the best cache performance because it has the
+highest hit rate. The vortex benchmark has the worst performance only scoring a
+hit 56.9 percent of the time.
+
+| Benchmark | Inst hit rate | Read hit rate | Write hit rate | Hit rate |
+|-----------|---------------|---------------|----------------|----------|
+| parser    |         92.8% |         87.2% |          63.4% |    84.3% |
+| equake    |         79.3% |         87.1% |          80.9% |    81.6% |
+| vortex    |         58.7% |         74.8% |          28.2% |    56.9% |
+: Cache performance with default settings {#tbl:cache_statistics_results}
+
+3.3 Determining benchmark working set size
+==========================================
+
+By increasing the cache size the amount of conflict and capacity misses will
+decrease. Because the cache has been warmed up in our benchmark, there should be
+very few compulsory misses. The cache size at the point when the hit rate
+stabilizes should give us an approximation of the program's working set. A
+visualization of the hit rate is presented in @fig:hit_rate_graph. We also
+tested the performance by increasing the associativity to 4 with the aim of
+decreasing conflict misses in order to get a better approximation. The graph
+suggests that the parser benchmark has the smallest working set at approximately
+60kB. The working set of the vortex benchmark is around 2MB. The equake
+benchmark has the largest working set. We estimate it is approximately 4MB.
+
+![Hit rate for different cache sizes and degrees of associativity](working_set_size.png){#fig:hit_rate_graph}
+
+3.4 Minimal instruction and data caches
+=======================================
+
+The performance using one cache line with different sizes is presented in
+@fig:minimal_cache_graph. Using one cache line will give us a hint of the
+spatial locality of the data. A line size of 32 bytes is quite small and will
+only perform well if the data is accessed sequentially. My intuition says that
+code is accessed mostly in this manner. The program counter will keep on
+increasing until a branch happens. The UltraSparc T1 processor has instructions
+of 32 bits, so without branches we should see a hit rate of 7/8 with a line size
+of 32 bits and a 31/32 hit rate for a line size of 128 @ultrasparct1. However,
+the performance doesn't come close to these values. Maybe there's more branches
+in the benchmarks than in typical code. One thing to note is that the
+instruction fetch hit rate almost doubles with the larger line size.
+
+The data reads and writes share the same cache, so it is very likely that they
+will interfere with each other. We see a big variance between the benchmarks.
+The parser has the most sequential reads, but the worst write performance. The
+equake benchmarks has the best write performance.
+
+The question was about comparing relative locality of data against instruction
+accesses. The data suggests that instruction accesses tend to follow the
+principles of temporal and spatial locality quite well. Data access is very much
+dependent on the benchmark. The parser benchmark achieves higher read hit rate
+than the code, but both the parser and vortex suffer of bad write performance.
+
+![Benchmark performance with one cache line](minimal_line_size.png){#fig:minimal_cache_graph}
+
+3.5 Collecting statistics from a cache hierarchy
+================================================
+
+The performance for the cache hierarchy for different sizes of L2 cache is given
+in @fig:cache_hierarchy_graph. The first question was to assess how effective
+the L2 cache is at collecting misses from the L1 level. We would argue that the
+L2 cache performs very well, because the hit rate is very high for every
+benchmark.
+
+The second question was related to how the size of the L2 cache changes its
+performance. We choose to analyze the performance in the vortex benchmark, which
+has the most variance in the L2 results. The small size of 8 KiB has
+significantly lower hit rate than the 500 kiB and 1 MiB L2 caches. The
+difference between 500 kiB and 1 MiB is negligible, so it doesn't make sense to
+invest in a larger L2 cache for this benchmark. Choosing between 8 kiB and 500
+kiB is a tradeoff between cost and performance. In the next section we will give
+a useful metric for assessing the performance between the different sizes.
+
+![Cache hierarchy performance](cache_hierarchy.png){#fig:cache_hierarchy_graph}
+
+Next we will calculate the average memory access time. We will use the
+following formula to calculate it.
+
+$$
+3 + L1_{\text{miss rate}}*(10 + L2_{\text{miss rate}}*200)
+$$
+
+The average access times are given in @tbl:amat_table. We calculated them by
+aggregating the statistics for all the benchmarks. As expected the access time
+gets shorter by increasing the cache size.
+
+| L2 cache size | Average memory access time (cycles) |
+|---------------|-------------------------------------|
+| 8 kiB         |                              6.1996 |
+| 500 kiB       |                              4.1795 |
+| 1 MiB         |                              4.1769 |
+: Average memory access time for the cache hierarcies {#tbl:amat_table}
+
+
+# Bibliography
+
+<!-- The bibliography gets populated here automatically thanks to
+pandoc-citeproc. -->
---
+++ reports/lab1_statistics.R
@@ -0,0 +1,138 @@
+library(ggplot2)
+library(dplyr)
+library(sitools)
+library(scales)
+library(tidyr)
+
+## 3.2
+
+df <- read.csv('../cache_statistics_excercise3-2.csv')
+
+df <- df %>%
+  mutate(cache_size=cache_lines * config_line_size) %>%
+  mutate(inst_hit_rate=1-stat_inst_fetch_miss/stat_inst_fetch) %>%
+  mutate(total_accesses=stat_data_read + stat_data_write + stat_inst_fetch) %>%
+  mutate(total_misses=stat_data_read_miss + stat_data_write_miss + stat_inst_fetch_miss) %>%
+  mutate(config_assoc=as.factor(config_assoc)) %>%
+  mutate(hit_rate=1-total_misses/total_accesses) %>%
+  mutate(hit_rate_percentage=percent(hit_rate)) %>%
+  mutate(read_hit_rate_percentage=percent(read_hit_rate)) %>%
+  mutate(inst_hit_rate_percentage=percent(inst_hit_rate)) %>%
+  mutate(write_hit_rate_percentage=percent(write_hit_rate))
+
+df %>%
+  filter(config_line_number ==  128) %>%
+  filter(config_assoc ==  1) %>%
+  arrange(desc(hit_rate)) %>%
+  select(Benchmark=benchmark,
+         "Inst hit rate"=inst_hit_rate_percentage,
+         "Read hit rate"=read_hit_rate_percentage,
+         "Write hit rate"=write_hit_rate_percentage,
+         "Hit rate"=hit_rate_percentage)
+
+## 3.3
+
+breaks <- c(4e3, 16e3, 6e4, 22e4, 7e5, 2e6, 8e6, 33e6)
+
+ggplot(data=df,
+       aes(x=cache_size,
+           y=hit_rate,
+           group=interaction(benchmark, config_assoc),
+           shape=config_assoc,
+           colour=benchmark)) +
+  geom_line() +
+  scale_x_log10(breaks=breaks, labels={function (x) f2si(x, unit='B')}) +
+  ylab("Hit rate") +
+  xlab("Cache size (bytes)") +
+  scale_colour_discrete(name="Benchmark") +
+  scale_shape_discrete(name="Associativity") +
+  geom_point(size=1, fill="white")
+
+
+## 3.4
+
+minimal_cache <- read.csv('../cache_statistics_excercise3-4.csv')
+minimal_cache <- minimal_cache %>%
+  mutate(data_read_hit_rate=1-data_read_miss/data_read) %>%
+  mutate(data_write_hit_rate=1-data_write_miss/data_write) %>%
+  mutate(inst_fetch_hit_rate=1-inst_fetch_miss/inst_fetch)
+minimal_cache <- gather(minimal_cache, variable, hit_rate,
+                        c(data_write_hit_rate,
+                          data_read_hit_rate,
+                          inst_fetch_hit_rate))
+select(minimal_cache, benchmark, line_size, variable, hit_rate)
+
+minimal_cache <- mutate(minimal_cache,
+                        line_size=as.factor(line_size),
+                        access_type=as.factor(variable))
+
+levels(minimal_cache$access_type) <- c("Read", "Write", "Instruction")
+
+ggplot(data=minimal_cache, aes(x=benchmark,
+                               y=hit_rate,
+                               fill=line_size)) +
+  geom_col(position='dodge') +
+  scale_y_continuous(labels=percent, limits=c(0,1)) +
+  scale_fill_discrete(name="Line size (bytes)") +
+  xlab("Benchmark") +
+  ylab("Hit rate") +
+  facet_wrap(~access_type)
+
+## 3.5
+
+cache_hierarchy <- read.csv('../cache_statistics_excercise3-5.csv')
+
+cache_hierarchy_before_gather <- cache_hierarchy %>%
+  mutate(ic.hit_rate=1-ic.stat_inst_fetch_miss/ic.stat_inst_fetch,
+         dc.hit_rate=1-(dc.stat_data_read_miss+dc.stat_data_write_miss)/(dc.stat_data_read + dc.stat_data_write),
+         lev2c.total_misses=lev2c.stat_data_read_miss+lev2c.stat_data_write_miss+lev2c.stat_inst_fetch_miss,
+         lev2c.total_transactions=lev2c.stat_data_read+lev2c.stat_data_write+lev2c.stat_inst_fetch,
+         lev2c.miss_rate=lev2c.total_misses/lev2c.total_transactions,
+         lev2c.hit_rate=1-lev2c.miss_rate,
+         lev2c.cache_size=lev2c.config_line_number*lev2c.config_line_size,
+         lev1c.miss_rate=(ic.stat_inst_fetch_miss+dc.stat_data_read_miss+dc.stat_data_write_miss)/(dc.stat_data_read + dc.stat_data_write + ic.stat_inst_fetch),
+         lev1c.hit_rate=1-lev1c.miss_rate,
+         average_memory_access_time=lev1c.hit_rate*3 + lev1c.miss_rate*(lev2c.hit_rate*10 + lev2c.miss_rate*200))
+
+cache_hierarchy <- gather(cache_hierarchy_before_gather, variable, hit_rate,
+                          c(ic.hit_rate,
+                            dc.hit_rate,
+                            lev2c.hit_rate))
+
+cache_hierarchy <- mutate(cache_hierarchy,
+                          lev2c.cache_size=as.factor(lev2c.cache_size),
+                          cache_type=as.factor(variable))
+
+levels(cache_hierarchy$cache_type) <- c("L1 data cache",
+                                        "L1 instruction cache",
+                                        "L2 cache")
+
+
+select(cache_hierarchy, benchmark, lev2c.cache_size, variable, hit_rate)
+
+ggplot(data=cache_hierarchy, aes(x=benchmark,
+                               y=hit_rate,
+                               fill=lev2c.cache_size)) +
+  geom_col(position='dodge') +
+  scale_y_continuous(labels=percent, limits=c(0,1)) +
+  scale_fill_discrete(name="L2 cache size (bytes)",
+                      labels=c('8 kiB', '500 kiB', '1 MiB')) +
+  xlab("Benchmark") +
+  ylab("Hit rate") +
+  facet_wrap(~cache_type)
+
+## Horrible violation of DRY for 3.5 step 3
+group_by(cache_hierarchy_before_gather, lev2c.cache_size) %>%
+  summarize_at(vars(contains("stat_")),
+               sum) %>%
+  mutate(ic.hit_rate=1-ic.stat_inst_fetch_miss/ic.stat_inst_fetch,
+         dc.hit_rate=1-(dc.stat_data_read_miss+dc.stat_data_write_miss)/(dc.stat_data_read + dc.stat_data_write),
+         lev2c.total_misses=lev2c.stat_data_read_miss+lev2c.stat_data_write_miss+lev2c.stat_inst_fetch_miss,
+         lev2c.total_transactions=lev2c.stat_data_read+lev2c.stat_data_write+lev2c.stat_inst_fetch,
+         lev2c.miss_rate=lev2c.total_misses/lev2c.total_transactions,
+         lev2c.hit_rate=1-lev2c.miss_rate,
+         lev1c.miss_rate=(ic.stat_inst_fetch_miss+dc.stat_data_read_miss+dc.stat_data_write_miss)/(dc.stat_data_read + dc.stat_data_write + ic.stat_inst_fetch),
+         lev1c.hit_rate=1-lev1c.miss_rate,
+         average_memory_access_time=format(round(3 + lev1c.miss_rate*(10 + lev2c.miss_rate*200), 4), nsmall=4),
+         cache_size=c("8 kiB", "500 kiB", "1 MiB")) %>%
+  select("L2 cache size"=cache_size, "Average memory access time (cycles)"=average_memory_access_time)
