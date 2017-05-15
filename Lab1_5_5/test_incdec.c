0165dea02eb38445cdec78bca8c889d8b3d68c6d
Yrkkö Äkkijyrkkä <yrkko.akkijyrkka@gmail.com>
2017-05-15 12:55:03 +0200

    Atomic inc/dec

--- Lab1_5_5/test_incdec.c
+++ Lab1_5_5/test_incdec.c
@@ -16,36 +16,45 @@
 #include "lab1.5_asm.h"
 
 static void
-increase(int thread, int iterations, volatile int *data)
-{
-        /* TASK: Implement a loop that increments *data by 1 using
-         * non-atomic increment instructions. See lab2_asm.h.
-         */
+increase(int thread, int iterations, volatile int *data) {
+    /* TASK: Implement a loop that increments *data by 1 using
+     * non-atomic increment instructions. See lab2_asm.h.
+     */
+    for (int i = 0; i < iterations; i++) {
+        asm_inc_int32((int32_t *) data);
+    }
+
 }
 
 static void
-decrease(int thread, int iterations, volatile int *data)
-{
-        /* TASK: Implement a loop that decrements *data by 1 using
-         * non-atomic decrement instructions. See lab2_asm.h.
-         */
+decrease(int thread, int iterations, volatile int *data) {
+    /* TASK: Implement a loop that decrements *data by 1 using
+     * non-atomic decrement instructions. See lab2_asm.h.
+     */
+    for (int i = 0; i < iterations; i++) {
+        asm_dec_int32((int32_t *) data);
+    }
 }
 
 
 static void
-increase_atomic(int thread, int iterations, volatile int *data)
-{
-        /* TASK: Implement a loop that increments *data by 1 using
-         * atomic increment instructions. See lab2_asm.h.
-         */
+increase_atomic(int thread, int iterations, volatile int *data) {
+    /* TASK: Implement a loop that increments *data by 1 using
+     * atomic increment instructions. See lab2_asm.h.
+     */
+    for (int i = 0; i < iterations; i++) {
+        asm_atomic_inc_int32((int32_t *) data);
+    }
 }
 
 static void
-decrease_atomic(int thread, int iterations, volatile int *data)
-{
-        /* TASK: Implement a loop that decrements *data by 1 using
-         * atomic decrement instructions. See lab2_asm.h.
-         */
+decrease_atomic(int thread, int iterations, volatile int *data) {
+    /* TASK: Implement a loop that decrements *data by 1 using
+     * atomic decrement instructions. See lab2_asm.h.
+     */
+    for (int i = 0; i < iterations; i++) {
+        asm_atomic_dec_int32((int32_t *) data);
+    }
 }
 
 test_impl_t test_impl_incdec_atomic = {
@@ -53,7 +62,7 @@ test_impl_t test_impl_incdec_atomic = {
         .desc = "Modify a shared variable using inc/dec instructions",
 
         .num_threads = 2,
-        .test = { &increase, &decrease }
+        .test = {&increase, &decrease}
 };
 
 test_impl_t test_impl_incdec_no_atomic = {
@@ -61,7 +70,7 @@ test_impl_t test_impl_incdec_no_atomic = {
         .desc = "Modify a shared variable using atomic inc/dec instructions",
 
         .num_threads = 2,
-        .test = { &increase_atomic, &decrease_atomic }
+        .test = {&increase_atomic, &decrease_atomic}
 };
 
 /*
